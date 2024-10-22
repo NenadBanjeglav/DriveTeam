5fd6e66fa00837e7d248663bdd242f70adbf04b0
NenadBanjeglav <nenadmobmail@gmail.com>
2024-10-22 17:38:07 +0200

    test

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { InfiniteMovingCards } from "@/components/ui/IniniteMovingCards";
 import { testimonials } from "@/lib/data";
-import Image from "next/image";
 
 const Testimonials = () => {
   return (
@@ -12,7 +11,7 @@ const Testimonials = () => {
       <h2 className="mb-10 text-center text-4xl font-bold italic text-teal-600 md:mb-20">
         Šta kažu naši polaznici?
       </h2>
-      <div className="max-w-2xl">
+      {/* <div className="max-w-2xl">
         <Image
           src="/testimonials.jpg"
           width={400}
@@ -20,7 +19,7 @@ const Testimonials = () => {
           alt="Slika direktora"
           className="rounded-full object-cover shadow-2xl"
         />
-      </div>
+      </div> */}
       <div className="mx-auto mb-16 flex max-w-7xl flex-col-reverse items-center justify-center gap-8 lg:flex-row lg:gap-28">
         <div className="my-8 flex basis-2/4 flex-col items-center justify-center">
           <InfiniteMovingCards
